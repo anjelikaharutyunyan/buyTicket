02c1363b5c7cc37aa4b55822e5995fdf6f73dabf
Angelica Harutyunyan <anjelikaharutyunyan03@gmail.com>
2024-08-18 00:48:33 +0400

    custom hook for filtering soonest 3 events

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import TicketCard from '../../components/TicketCard/TicketCard';
-import { tickets } from './../../components/TicketCard/constants'
+import { tickets } from './../../components/TicketCard/constants';
+import Calendar from '../../components/Calendar/Calendar';
+import useSoonestDates from '../../hooks/useSoonestDate';
+
 
 const Home = () => {
   const [likedTickets, setLikedTickets] = useState({});
+  const soonestTickets = useSoonestDates(tickets);
 
   const handleLikeTicket = (title) => {
     setLikedTickets(prev => ({
@@ -13,15 +17,37 @@ const Home = () => {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', gap: '30px', marginTop: '100px' }}>
-      {tickets.map((ticket, index) => (
-        <TicketCard
-          key={index}
-          ticket={ticket}
-          isLiked={!!likedTickets[ticket.title]}
-          onLike={() => handleLikeTicket(ticket.title)}
-        />
-      ))}
+    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '100px', justifyContent: 'center', paddingInline: '40px', gap: '30px' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Calendar />
+        <div>
+          <h1>Soon</h1>
+          <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', gap: '30px' }}>
+            {soonestTickets.map((ticket, index) => (
+              <TicketCard
+                key={index}
+                ticket={{ ...ticket, date: ticket.formattedDate }} 
+                isLiked={!!likedTickets[ticket.title]}
+                onLike={() => handleLikeTicket(ticket.title)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'column', backgroundColor: 'lightgray' }}>
+        <h1 style={{paddingLeft: '55px'}}>EVENTS</h1>
+        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', gap: '30px' }}>
+          {tickets.map((ticket, index) => (
+            <TicketCard
+              key={index}
+              ticket={ticket}
+              isLiked={!!likedTickets[ticket.title]}
+              onLike={() => handleLikeTicket(ticket.title)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
